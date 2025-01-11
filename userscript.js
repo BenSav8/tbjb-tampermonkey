49637eb27a7aa5bf3cd6266204d186e05c575244
BenSav <153027450+BenSav8@users.noreply.github.com>
2025-01-11 12:46:38 -0300

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -12,5 +12,6 @@
 fetch('https://dell-optiplex-790.github.io/tbjb/tb.js')
 	.then((response) => response.text())
 	.then((tbjb) => {
+		localStorage.setItem('.config/trollbox/tbjb', tbjb)
     document.querySelector("#trollbox_nick_btn").innerText=localStorage.getItem(".config/trollbox/_nick");socket.disconnect(),document.body.querySelectorAll(".trollbox_line").forEach(e=>e.parentElement.removeChild(e));eval(localStorage.getItem(".config/trollbox/tbjb"))
 	});
