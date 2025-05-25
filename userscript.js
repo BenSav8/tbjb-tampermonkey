52f65f4a9081d398a86bc0ca1c87bd6bc9d86c29
BenSav <153027450+BenSav8@users.noreply.github.com>
2025-05-25 12:23:39 -0300

    timeout to avoid disconnection

--- userscript.js
+++ userscript.js
@@ -13,5 +13,7 @@ fetch('https://bensav8.github.io/tbjb/tb.js')
 	.then((response) => response.text())
 	.then((tbjb) => {
 		localStorage.setItem('.config/trollbox/tbjb', tbjb)
+    setTimeout(function() {
     document.querySelector("#trollbox_nick_btn").innerText=localStorage.getItem(".config/trollbox/_nick");socket.disconnect(),document.body.querySelectorAll(".trollbox_line").forEach(e=>e.parentElement.removeChild(e));eval(localStorage.getItem(".config/trollbox/tbjb"))
-	});
+    }, 1100)
+    });
